f602d770473e54ccc996970ae525eaad80365565
Jacob Holtom <jacob@holtom.me>
2017-08-08 23:12:24 -0600

    Finished title page and templating

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from flask import Flask
 from flask import render_template
+import serial
+
+#ser = serial.Serial('/dev/ttyUSB0')
+
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-	return render_template('index.html')
+        #ser.write('I')
+        #x = ser.readline()
+        x = "Ready for service"
+        if "Ready for service" in x:
+          	return render_template('index.html')
+        else:
+                return "Error Encountered, printing Arduino Response: " + x # Improve this so it shows a beautiful page and reports to blendtec
 
 @app.route("/flavor")
-def sel_flavor(flavor=flavor):
-	return flavor
+def sel_flavor():
+    return "Set flavor array to: "
 
 @app.route("/initialize")
 def init_arduino():
 	return "{ 'status': 'init' }"
+
+@app.route("/make")
+def make():
+        ser.write('M')
+        # Parse Flavor Array here
+        ser.write()
+        return "{ 'status' : ''}"
