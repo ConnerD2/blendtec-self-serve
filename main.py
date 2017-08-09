194f853f05c316cc5acc3a6f703e24ef72ea9b55
Jacob Holtom <jacob@holtom.me>
2017-08-09 00:25:59 -0600

    Created working page system

--- main.py
+++ main.py
@@ -3,18 +3,38 @@ from flask import render_template
 import serial
 
 #ser = serial.Serial('/dev/ttyUSB0')
+#ser.write('I')
+#x = ser.readline()
+x = "Ready for service"
+if "Ready for service" in x:
+    ready = True
+else:
+    ready = False
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-        #ser.write('I')
-        #x = ser.readline()
-        x = "Ready for service"
-        if "Ready for service" in x:
-          	return render_template('index.html')
-        else:
-                return "Error Encountered, printing Arduino Response: " + x # Improve this so it shows a beautiful page and reports to blendtec
+    if ready == True:
+        return render_template('index.html')
+    else:
+        return "Error: " + x
+
+@app.route("/selection")
+def selector():
+    return render_template('select.html')
+
+@app.route("/health")
+def health():
+    return render_template('health.html')
+
+@app.route("/blending")
+def blending():
+    return render_template('blending.html')
+
+@app.route("/complete")
+def complete():
+    return render_template('complete.html')
 
 @app.route("/flavor")
 def sel_flavor():
@@ -22,11 +42,11 @@ def sel_flavor():
 
 @app.route("/initialize")
 def init_arduino():
-	return "{ 'status': 'init' }"
+    return "{ 'status': 'init' }"
 
 @app.route("/make")
 def make():
-        ser.write('M')
-        # Parse Flavor Array here
-        ser.write()
-        return "{ 'status' : ''}"
+    ser.write('M')
+    # Parse Flavor Array here
+    ser.write()
+    return "{ 'status' : ''}"
